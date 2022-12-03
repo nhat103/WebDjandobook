29cbece6126e89cecd7a1e7a36648d6b08198ce6
Nguyễn Nhật <ng.nhat103@gmail.com>
2022-12-03 16:26:56 +0700

    update chapter 8

--- chapter8/Exercise 8.03/media_example/views.py
+++ chapter8/Exercise 8.03/media_example/views.py
@@ -1,20 +1,15 @@
 import os
 from django.conf import settings
 from django.shortcuts import render
-from .forms import UploadForm
 
 
 def media_example(request):
     if request.method == 'POST':
-        form = UploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            save_path = os.path.join(
-                settings.MEDIA_ROOT, request.FILES["file_upload"].name)
+        save_path = os.path.join(
+            settings.MEDIA_ROOT, request.FILES["file_upload"].name)
 
         with open(save_path, "wb") as output_file:
             for name in request.FILES["file_upload"].chunks():
                 output_file.write(name)
-    else:
-        form = UploadForm()
 
     return render(request, "media-example.html")
--- media_project/media_example/urls.py
+++ media_project/media_example/urls.py
@@ -1,7 +1,7 @@
 """media_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,10 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
+import media_example.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('media_example.urls')),
+    path('media-example/', media_example.views.media_example)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
